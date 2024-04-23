7fab8475b3581ac2ff322817a924e5918b9f145c
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-23 17:00:23 +0200

    M4 - second btn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const slides = document.getElementsByClassName("slide");
-const next_btn = document.getElementById(`next_btn`);
+const next_btn = document.getElementById(`next-btn`);
+const prev_btn = document.getElementById(`prev-btn`);
+
 let active_img;
 
 next_btn.addEventListener(`click`, function () {
@@ -13,4 +15,17 @@ next_btn.addEventListener(`click`, function () {
 	}
 
 	slides[active_img + 1].classList.add(`active`);
+});
+
+prev_btn.addEventListener(`click`, function () {
+	for (let i = 0; i < slides.length; i++) {
+		const slide = slides[i];
+		console.log(slide);
+		if (slide.classList.contains(`active`)) {
+			slide.classList.remove(`active`);
+			active_img = i;
+		}
+	}
+
+	slides[active_img - 1].classList.add(`active`);
 });
