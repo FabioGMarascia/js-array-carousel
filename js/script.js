56b6aa1691f0f73d08a848d69f589cf533c1e4c9
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-23 16:54:02 +0200

    M3 - slide button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const slides = document.getElementsByClassName("slide");
+const next_btn = document.getElementById(`next_btn`);
 let active_img;
 
-for (let i = 0; i < slides.length; i++) {
-	const slide = slides[i];
-	console.log(slide);
-	if (slide.classList.contains(`active`)) {
-		slide.classList.remove(`active`);
-		active_img = i;
+next_btn.addEventListener(`click`, function () {
+	for (let i = 0; i < slides.length; i++) {
+		const slide = slides[i];
+		console.log(slide);
+		if (slide.classList.contains(`active`)) {
+			slide.classList.remove(`active`);
+			active_img = i;
+		}
 	}
-}
 
-slides[active_img + 1].classList.add(`active`);
+	slides[active_img + 1].classList.add(`active`);
+});
