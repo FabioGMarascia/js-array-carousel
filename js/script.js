15bf2a47cfe329b068e8233e66431c8ad4a875cc
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-23 17:43:04 +0200

    Bonus 1

--- js/script.js
+++ js/script.js
@@ -2,19 +2,25 @@ const slides = document.getElementsByClassName("slide");
 const next_btn = document.getElementById(`next-btn`);
 const prev_btn = document.getElementById(`prev-btn`);
 
-let active_img;
+let active_img_i;
 
 next_btn.addEventListener(`click`, function () {
 	for (let i = 0; i < slides.length; i++) {
 		const slide = slides[i];
-		console.log(slide);
+
 		if (slide.classList.contains(`active`)) {
 			slide.classList.remove(`active`);
-			active_img = i;
+			active_img_i = i;
 		}
 	}
 
-	slides[active_img + 1].classList.add(`active`);
+	let next_img_i = active_img_i + 1;
+
+	if (next_img_i >= slides.length) {
+		slides[0].classList.add(`active`);
+	} else {
+		slides[next_img_i].classList.add(`active`);
+	}
 });
 
 prev_btn.addEventListener(`click`, function () {
@@ -23,9 +29,15 @@ prev_btn.addEventListener(`click`, function () {
 		console.log(slide);
 		if (slide.classList.contains(`active`)) {
 			slide.classList.remove(`active`);
-			active_img = i;
+			active_img_i = i;
 		}
 	}
 
-	slides[active_img - 1].classList.add(`active`);
+	let prev_img_i = active_img_i - 1;
+
+	if (prev_img_i < 0) {
+		slides[3].classList.add(`active`);
+	} else {
+		slides[prev_img_i].classList.add(`active`);
+	}
 });
